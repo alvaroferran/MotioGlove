72cc23f3cf13cada921650c0ecaed2ccc79be943
alvaroferran <alvaroferran@gmail.com>
2016-06-15 16:27:06 +0200

    refactored code

--- Software/MotioGlove.ino
+++ Software/MotioGlove.ino
@@ -10,66 +10,76 @@ SoftwareSerial mySerial(4, 3); // RX, TX
 BNO055 sensor(A);
 
 int pinButton=10;
-int btn;
-float x,y,z;
 
-int sensorMin=10;
-int sensorMax=40;
 
 
 void setup(){
     Wire.begin();
     mySerial.begin(19200);
+    Serial.begin(19200);
     sensor.init();
     pinMode(pinButton, INPUT_PULLUP);
 }
 
 
-float map(float vx, float v1, float v2, float n1, float n2){
-    // v1 start of range
-    // v2 end of range
-    // vx the starting number between the range
-    float percentage = (vx-v1)/(v2-v1);
-    // n1 start of new range
-    // n2 end of new range
+
+double map(double vx, double v1, double v2, double n1, double n2){
+    // v1 start of range, v2 end of range, vx the starting number between the range
+    double percentage = (vx-v1)/(v2-v1);
+    // n1 start of new range, n2 end of new range
     return (n2-n1)*percentage+n1;
 }
 
 
-void loop(){    
-    sensor.readEul();
 
+void normalizeAxes(double *aAxes){
+    int sensorMin=10, sensorMax=40;
+    double aX=aAxes[0];
+    double aY=aAxes[1];
+    //Resting zone
+    if(aX<sensorMin && aX>-sensorMin) aX=0;
+    if(aY<sensorMin && aY>-sensorMin) aY=0;
+    //Map from angles to aX values between -1 and 1
+    if(aX<-sensorMin) aX=map(aX,-sensorMin,-sensorMax,0,1);
+    if(aX>sensorMin)  aX=map(aX,sensorMin,sensorMax,0,-1);
+    if(aY<-sensorMin) aY=map(aY,-sensorMin,-sensorMax,0,1);
+    if(aY>sensorMin)  aY=map(aY,sensorMin,sensorMax,0,-1);
+    aY=map(aY,-1,1,1,-1);
+    //Limit maximum values in all axes
+    if(aX<-1) aX=-1;
+    if(aX>1)  aX=1;
+    if(aY<-1) aY=-1;
+    if(aY>1)  aY=1;
+
+    aAxes[0]=aX;
+    aAxes[1]=aY;
+}
+
+
+
+void loop(){
+    double x,y,z;
+    int btn;
+
+    sensor.readEul();
     x=sensor.euler.y;
     y=sensor.euler.z;
-    
-    btn=digitalRead(pinButton);    //roll
 
-    //Resting zone
-    if(x<sensorMin && x>-sensorMin) x=0; 
-    if(y<sensorMin && y>-sensorMin) y=0; 
+    double axes[2];
+    axes[0]=x; axes[1]=y;
+    normalizeAxes(&axes[0]);
+    x=axes[0]; y=axes[1];
 
-    //Map from angles to values between -1 and 1
-    if(x<-sensorMin) x=map(x,-sensorMin,-sensorMax,0,1);
-    if(x>sensorMin) x=map(x,sensorMin,sensorMax,0,-1);
-    if(y<-sensorMin) y=map(y,-sensorMin,-sensorMax,0,1);
-    if(y>sensorMin) y=map(y,sensorMin,sensorMax,0,-1);
-    y=map(y,-1,1,1,-1);
-    
-    //Limit maximum values in all axes
-    if(x<-1) x=-1;
-    if(x>1) x=1;
-    if(y<-1) y=-1;
-    if(y>1) y=1;
-
+    btn=digitalRead(pinButton);
     //If button pressed set roll angle to yaw
-    if(btn1==0){
+    if(btn==0){
         z=y;
         y=0;
         x=0;
     }
     else z=0;
 
-    //Send values
+    // Send values
     mySerial.print("(");
     mySerial.print(x,2);
     mySerial.print(";");
@@ -80,4 +90,3 @@ void loop(){
 
     delay(100);
 }
-
